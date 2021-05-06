91cb622c64fb4217cd04210366eb4cc2531dfa6a
laurent <laurent.duchaussoy59@gmail.com>
2021-05-07 00:09:18 +0200

    feat(Mongo): add mongo Connection + keypass
    create db from https://cloud.mongodb.com/

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { secrets } from '../keypass';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      `mongodb+srv://admin:${secrets.mongoPwd}@${secrets.mongoUrl}/${secrets.mongoDatabase}?retryWrites=true&w=majority`,
+      {
+        connectionFactory: (connection) => {
+          connection.then(() => {
+            console.log('connection OK');
+          });
+          connection.catch(() => {
+            console.log('connection KO');
+          });
+          return connection;
+        },
+      },
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
